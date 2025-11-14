8bfe4ecafbaf463922193ddf26a78624ce71f36b
einkv <xxhyk413912mlxx@gmail.com>
2025-11-14 23:26:27 +0800

    fix: user list + invalid session + worker timeout

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 ﻿from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, emit
 import os
+import eventlet
+
+eventlet.monkey_patch()  # 關鍵！修復 worker timeout
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test-secret'
@@ -14,38 +17,44 @@ def index():
 
 @socketio.on('join')
 def on_join(data):
-    username = data['username']
-    room = data['room']
+    username = data.get('username', '匿名')
+    room = data.get('room', 'lobby')
+    sid = request.sid
+    
     join_room(room)
     
     if room not in rooms:
-        rooms[room] = {'users': []}
+        rooms[room] = []
     
-    current_sid = request.sid  # 正確的 SocketIO sid
+    # 移除舊的同名用戶
+    rooms[room] = [u for u in rooms[room] if u['name'] != username]
     
-    # 避免重複
-    if not any(u['name'] == username for u in rooms[room]['users']):
-        rooms[room]['users'].append({'name': username, 'sid': current_sid})
+    # 加入新用戶
+    rooms[room].append({'name': username, 'sid': sid})
     
-    emit('status', {'msg': f'{username} 加入房間！'}, room=room)
-    emit('user_list', [u['name'] for u in rooms[room]['users']], room=room)
+    # 廣播給房間所有人
+    emit('status', f'{username} 加入房間！', room=room)
+    emit('user_list', [u['name'] for u in rooms[room]], room=room, include_self=True)
 
 @socketio.on('send_message')
 def on_message(data):
-    room = data['room']
-    msg = data['message']
-    username = data['username']
-    emit('new_message', {'username': username, 'message': msg}, room=room)
+    room = data.get('room')
+    msg = data.get('message')
+    username = data.get('username')
+    if room in rooms:
+        emit('new_message', {'username': username, 'message': msg}, room=room)
 
 @socketio.on('disconnect')
 def on_disconnect():
     sid = request.sid
     for room in list(rooms.keys()):
-        rooms[room]['users'] = [u for u in rooms[room]['users'] if u['sid'] != sid]
-        if not rooms[room]['users']:
-            del rooms[room]
-        else:
-            emit('user_list', [u['name'] for u in rooms[room]['users']], room=room)
+        before_count = len(rooms[room])
+        rooms[room] = [u for u in rooms[room] if u['sid'] != sid]
+        if len(rooms[room]) < before_count:  # 有人離開
+            if rooms[room]:
+                emit('user_list', [u['name'] for u in rooms[room]], room=room)
+            else:
+                del rooms[room]
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
